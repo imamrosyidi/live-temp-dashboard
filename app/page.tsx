c8605193f6a570b429ccc67cd82e9f5b80b9de5d
imamrosyidi <68006342+imamrosyidi@users.noreply.github.com>
2024-10-26 17:24:24 +0700

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="">
+      Hellow world
+      <Button>My Button</Button>
+    </div>
+  );
+}
